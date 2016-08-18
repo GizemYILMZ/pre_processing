274e84068c1f4a78c6634ccb72c3fe03447295f9
Gizem <g.yilmaz68@gmail.com>
2016-08-18 22:21:31 +0300

    Adding all data to corpus and creating term-document matrix
    18.08.2016

--- PreProcessing.py
+++ PreProcessing.py
@@ -1,33 +1,71 @@
 # -*- coding: utf-8 -*-
 import nltk
+import sklearn
+import string
+import sklearn.feature_extraction
 from nltk.tokenize import word_tokenize
 from stop_words import get_stop_words
-import sklearn
-import sklearn.feature_extraction
 from TurkishStemmer import TurkishStemmer
+from os import walk
 
-stop_words = get_stop_words('tr')
-stemmer = TurkishStemmer()
 vectorizer = sklearn.feature_extraction.text.CountVectorizer(min_df=1)
 
+stemmed_list = " "
+corpus = []
+
 def stem_word(word):
-    stemmed_word= stemmer.stem(word)
-    print(stemmed_word)
+    global stemmed_list
+    stemmer = TurkishStemmer()
+    stemmed_word = stemmer.stem(word)
+    stemmed_list = stemmed_list + " " + stemmed_word
+    return stemmed_list
 
 def remove_stop_words(word):
-    if word.lower() not in stop_words:
+    stop_words = get_stop_words('tr')
+    exclude = set(string.punctuation)
+    if word.lower() not in stop_words and word not in exclude:
         stem_word(word)
-        return;
+    return;
 
 def tokenize_line(line):
-        word_list=word_tokenize(line);
-        return word_list;
+    word_list = word_tokenize(line);
+    return word_list;
 
-i=0
-with open('C:/1150haber/raw_texts/ekonomi/1.txt', encoding='utf-8') as file:
-       for line in file:
-            word_list=tokenize_line(line)
+
+def create_corpus(file_path):
+    global corpus
+    with open(file_path, encoding='utf-8') as open_file:
+        for line in open_file:
+            word_list = tokenize_line(line)
             for i in range(len(word_list)):
                 remove_stop_words(word_list[i])
+    open_file.close()
+    corpus.append(stemmed_list)
+    return
+
+
+# find file names in a directory
+f = []
+for (dirpath, dirnames, filenames) in walk('C:/1150haber/raw_texts/ekonomi'):
+    f.extend(filenames)
+    break
+
+# create corpus for all files in a directory
+for i in range(len(filenames)):
+    stemmed_list = ''
+    file_path=dirpath + "/" + filenames[i]
+    print(file_path)
+    create_corpus(file_path)
+
+target = open('target.txt', 'w')
+
+# create document - term matrix
+X = vectorizer.fit_transform(corpus).toarray()
+
+print('X: {0}' .format(X))
+print('vectorizer.vocabulary_: {0}'.format(vectorizer.vocabulary_))
+target.write('X: {0}'.format(X))
+target.write(format(vectorizer.vocabulary_))
+
 
 
