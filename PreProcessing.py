722068fb8d7b40bf3718578589e78cd3c7249451
Gizem <g.yilmaz68@gmail.com>
2016-09-07 18:18:03 +0300

    Some problems fixed.
    18.08.2016

--- PreProcessing.py
+++ PreProcessing.py
@@ -1,30 +1,42 @@
 # -*- coding: utf-8 -*-
-import nltk
-import sklearn
+
 import string
 import sklearn.feature_extraction
+from sklearn.feature_extraction.text import CountVectorizer
 from nltk.tokenize import word_tokenize
 from stop_words import get_stop_words
 from TurkishStemmer import TurkishStemmer
 from os import walk
+import pandas as pd
+from nltk.book import *
+import snowballstemmer
 
 vectorizer = sklearn.feature_extraction.text.CountVectorizer(min_df=1)
-
 stemmed_list = " "
 corpus = []
-def stem_word(word):
-    global stemmed_list
-    stemmer = TurkishStemmer()
-    stemmed_word = stemmer.stem(word)
-    stemmed_list = stemmed_list + " " + stemmed_word
-    return stemmed_list
+word_list = []
 
-def remove_stop_words(word):
+
+def remove_stop_words(stemmed_word):
+    global stemmed_list
+    global word_list
     stop_words = get_stop_words('tr')
     exclude = set(string.punctuation)
-    if word.lower() not in stop_words and word not in exclude:
-        stem_word(word)
+    if stemmed_word.lower() not in stop_words:
+        if stemmed_word.lower() not in exclude:
+            stemmed_list = stemmed_list + " " + stemmed_word
+            word_list.append(stemmed_word.lower())
+        return;
     return;
+
+
+def stem_word(word):
+  # stemmer = TurkishStemmer()
+    stemmer2 = snowballstemmer.stemmer('turkish');
+    stemmed_word = stemmer2.stemWord(word)
+    remove_stop_words(stemmed_word)
+    return;
+
 
 def tokenize_line(line):
     word_list = word_tokenize(line);
@@ -37,34 +49,47 @@ def create_corpus(file_path):
         for line in open_file:
             word_list = tokenize_line(line)
             for i in range(len(word_list)):
-                remove_stop_words(word_list[i])
+                stem_word(word_list[i])
     open_file.close()
     corpus.append(stemmed_list)
+    new_path=file_path + "kopya"
+    print(new_path)
+    target = open(new_path, 'w', encoding='utf-8')
+    target.write(stemmed_list)
+    target.close()
     return
 
 
 # find file names in a directory
 f = []
-for (dirpath, dirnames, filenames) in walk('C:/1150haber/raw_texts/ekonomi'):
+for (dirpath, dirnames, filenames) in walk('C:/1150haber/raw_texts/test'):
     f.extend(filenames)
     break
 
 # create corpus for all files in a directory
 for i in range(len(filenames)):
     stemmed_list = ''
-    file_path=dirpath + "/" + filenames[i]
+    word_list.clear()
+    file_path = dirpath + "/" + filenames[i]
     print(file_path)
     create_corpus(file_path)
+    # fdist1 = FreqDist(word_list)
+    # print(fdist1.most_common(10))
 
 target = open('target.txt', 'w')
 
 # create document - term matrix
 X = vectorizer.fit_transform(corpus).toarray()
 
-print('X: {0}' .format(X))
+print('X: {0}'.format(X))
 print('vectorizer.vocabulary_: {0}'.format(vectorizer.vocabulary_))
 target.write('X: {0}'.format(X))
 target.write(format(vectorizer.vocabulary_))
 
+pd.options.display.max_rows = 150
+pd.options.display.max_columns= 7483
+countvec = CountVectorizer()
+A = pd.DataFrame(countvec.fit_transform(corpus).toarray(), columns=countvec.get_feature_names())
+print(A)
 
 
