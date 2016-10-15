2cb29344b91996f66dccca67e55e45fe6750eddc
Gizem <g.yilmaz68@gmail.com>
2016-10-15 14:38:54 +0300

    Update - 15.10.2016

--- Analysis.py
+++ Analysis.py
@@ -1,34 +1,90 @@
+
 import os
-import pandas as pd
-import numpy as np
-import sklearn
+# import pandas as pd
+# import numpy as np
+# import sklearn
 import nltk
-from nltk.corpus import stopwords
-from nltk.stem.snowball import SnowballStemmer
+# from nltk.corpus import stopwords
+# from nltk.stem.snowball import SnowballStemmer
 import string
-from collections import Counter
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import LabelEncoder
-from sklearn.linear_model import SGDClassifier
+# from collections import Counter
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.pipeline import Pipeline
+# from sklearn.preprocessing import LabelEncoder
+# from sklearn.linear_model import SGDClassifier
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import classification_report as clsr
+# from sklearn.metrics import classification_report as clsr
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import cross_validation
 from sklearn.cross_validation import KFold, cross_val_score
-from NLTKPreprocessor import NLTKPreprocessor
+# from NLTKPreprocessor import NLTKPreprocessor
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
-from sklearn import preprocessing
-#import pylab as pl
+# from sklearn import preprocessing
+# import pylab as pl
 from sklearn import svm
+# from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn import metrics
+import pandas
 
-class MyEval:
-    accuracy=0.0
-    AUC=0.0
-    F1=0.0
+class MyEvaluation(object):
     def __init__(self):
-        return
+        self._accuracy = 0.0
+        self._AUC = 0.0
+        self._macroF1 = 0.0
+        self._microF1 = 0.0
+        self._F1 = []
+        self._Precision = []
+        self._Recall = []
+        self._Conf_Matrix= pandas.DataFrame
+
+    def _getAccuracy(self):
+        return self._accuracy
+
+    def _setAccuracy(self,value):
+        self._accuracy=value
+
+    def _getAUC(self):
+        return self._AUC
+
+    def _setAccuracy(self, value):
+        self._AUC = value
+
+    def _getAccuracy(self):
+        return self._microF1
+
+    def _setAccuracy(self, value):
+        self._microF1 = value
+
+    def _getAccuracy(self):
+        return self._macroF1
+
+    def _setAccuracy(self, value):
+        self._macroF1 = value
+
+    def _getPrecision(self):
+        return self._Precision
+
+    def _setPrecision(self, value):
+        self._Precision = value
+
+    def _getRecall(self):
+        return self._Recall
+
+    def _setRecall(self, value):
+        self._Recall = value
+
+    def _getConfusionMatrix(self):
+        return self._Conf_Matrix
+
+    def _setConfusionMatrix(self, value):
+        self._Conf_Matrix = value
+
+
+
+
 # ------------Preprocessing Data-------------------
 
 def stemmerTrFps6(term):
@@ -65,7 +121,7 @@ def evaluate_testOnTrainingSet(dataset,classlabels,classifier):
     y_test = y_train
     predictions = classifier.predict(X_test)
     evalReport = classification_report(y_test, predictions, target_names=classifier.classes_)
-    print(evalReport)
+    # print(evalReport)
     return
 
 
@@ -75,22 +131,35 @@ def evaluate_nFoldCV(dataset,classlabels,classifier,n):
     X_test = X_train
     y_test = y_train
     # k_fold = KFold(len(y_test), n_folds=n, shuffle=True, random_state=0)
-    scores = cross_val_score(classifier, X_test, y_test, cv=n, n_jobs=1)
-    print(scores)
-    print("Mean score: {0:.3f} (+/-{1:.3f})".format(scores.mean(), scores.std()))
-    return
+    predictions=cross_validation.cross_val_predict(classifier,X_train,y_test,cv=n)
+    # scores = cross_val_score(classifier, X_test, y_test, cv=n, n_jobs=1)
+    accuracy= metrics.accuracy_score(y_test, predictions)
+    precision = precision_score(y_test, predictions, average=None)
+    recall = recall_score(y_test, predictions, average=None)
+    cm = confusion_matrix(y_test, predictions)
+    myEval = MyEvaluation()
+    myEval._setAccuracy(accuracy.mean())
+    myEval._setPrecision(precision)
+    myEval._setRecall(recall)
+    return myEval
+
 
 
 def evaluate_trainTestSplit(dataset,classLabels,classifier,testPercantage):
     X_train, X_test, y_train, y_test = cross_validation.train_test_split(dataset, classLabels, test_size=testPercantage,random_state=0)
     predictions = classifier.predict(X_test)  # Perform classification on an array of test vectors X.
-    evalReport = classification_report(y_test, predictions, target_names=classifier.classes_)
+    # evalReport = classification_report(y_test, predictions, target_names=classifier.classes_)
+    precision=precision_score(y_test, predictions,average=None)
+    recall=recall_score(y_test, predictions,average=None)
+    # print(precision_recall_fscore_support(y_test, predictions, average='micro',labels =classLabels))
     cm = confusion_matrix(y_test, predictions)
-    print(evalReport)
-    print("Confusion matrix:")
     accuracy = classifier.score(X_test, y_test)
-    print(accuracy)
-    return
+    myEval = MyEvaluation()
+    myEval._setAccuracy(accuracy)
+    myEval._setPrecision(precision)
+    myEval._setRecall(recall)
+    myEval._setConfusionMatrix(cm)
+    return myEval
 
 
 path = 'data/1150haber/'
@@ -106,33 +175,52 @@ for subdir, dirs, files in os.walk(path):
 
 
 tfidf = TfidfVectorizer(tokenizer=tokenize, preprocessor=preprocess, lowercase=True, stop_words='english')
-docTermMatrix = tfidf.fit_transform((open(f,encoding='utf8').read() for f in fileNames))
+docTermMatrix = tfidf.fit_transform((open(f,encoding=None).read() for f in fileNames))
+dataset=docTermMatrix
 
 #print(docTermMatrix[1,:].todense())
 #print(type(docTermMatrix))
 #print(docTermMatrix.shape)
 #print(classLabels)
-dataset=docTermMatrix
 
 classifier =MultinomialNB().fit(dataset, classLabels)
 
-# test on training set
+# test on training set---------------------------------------------------------
+
 evaluate_testOnTrainingSet(dataset,classLabels,classifier)
 
-# train-test split 60%-40% with multinominal_nb
+# train-test split 60%-40% with multinominal_nb---------------------------------
+
 percentage=0.4
-print(" split 60%-40% with Multinominal NB")
-evaluate_trainTestSplit(dataset,classLabels,classifier,percentage)
 
-# nFold CV with multinominal_nb
+myEval_trainTestSplit_MB=evaluate_trainTestSplit(dataset,classLabels,classifier,percentage)
+print("\n split 60%-40% with Multinominal NB \n" )
+print("Accuracy :" , myEval_trainTestSplit_MB._getAccuracy())
+print("Precision:" , myEval_trainTestSplit_MB._getPrecision())
+print("Recall   :" , myEval_trainTestSplit_MB._getRecall())
+print("Confusion Matrix :\n " , myEval_trainTestSplit_MB._getConfusionMatrix())
+
+# nFold CV with multinominal_nb-------------------------------------------------
+
 n=10
-print(" 10 fold CV with Multinominal NB")
-evaluate_nFoldCV(dataset,classLabels,classifier,n)
+
+myEval_nFoldCV_MB=evaluate_nFoldCV(dataset,classLabels,classifier,n)
+print("\n 10 fold CV with Multinominal NB \n")
+print("Accuracy :", myEval_nFoldCV_MB._getAccuracy())
+print("Precision:", myEval_nFoldCV_MB._getPrecision())
+print("Recall   :", myEval_nFoldCV_MB._getRecall())
 
 
 
-# clf = svm.SVC(kernel='linear',C=1.0,decision_function_shape=None)
-# clf.fit(X_train, y_train)
-# prediction=clf.predict(X_test)
-# accuracy=clf.score(X_test,prediction)
-# print(accuracy)
+# train-test split 60%-40% with SVM--------------------------------
+
+clf = svm.SVC(kernel='linear',C=1.0,decision_function_shape=None)
+classifier = clf.fit(dataset , classLabels)
+myEval_trainTestSplit_SVM=evaluate_trainTestSplit(dataset,classLabels,classifier,percentage)
+print("\n split 60%-40% with SVM \n" )
+print("Accuracy :", myEval_trainTestSplit_SVM._getAccuracy())
+print("Precision:", myEval_trainTestSplit_SVM._getPrecision())
+print("Recall   :", myEval_trainTestSplit_SVM._getRecall())
+print("Confusion Matrix :\n " , myEval_trainTestSplit_SVM._getConfusionMatrix())
+
+
